2817fbeb4c4af25f1d8fa77ec465576e31a7f638
mxdws <tinodawsino@gmail.com>
2019-11-27 20:16:11 +0000

    Add eleventyignore file

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     return {
       templateFormats: [
+        "md",
         "css" // css is not yet a recognized template extension in Eleventy
       ]
     };
